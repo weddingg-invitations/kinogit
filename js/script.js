817d090292054e75edd4baa7b758dde934d10e3a
Edgar <83462313+weddingg-invitations@users.noreply.github.com>
2025-01-24 15:50:14 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,16 @@ function global__scripts() {
         })
     }
 
+    // run gane TicTacToe
+    let click_count = 0
+    document.querySelector(".move_info_cont").addEventListener('click', () => {
+        click_count++
+        console.log('click_count:', click_count);
+        if (click_count == 7) {
+            window.location.href = 'https://kinogit.us/pages/gameTicTacToe/game.html'
+        }
+    })
+
     // cookie_popup()
     // function cookie_popup() {
     //     if (document.getElementById('showMassag_cookie_on')) {
